83568fdc685d3ebff13fc3bd59e5ff927b791182
Aayushchauhan05 <140552699+Aayushchauhan05@users.noreply.github.com>
2024-10-26 19:49:41 +0530

    mentor listing api connected

--- frontend/src/app/marketplace/page.jsx
+++ frontend/src/app/marketplace/page.jsx
@@ -1,119 +1,99 @@
-"use client"
-import React, { useCallback, useState,useEffect } from 'react'
+"use client";
+import React, { useCallback, useState, useEffect } from 'react';
 import {
     Tooltip,
     TooltipContent,
     TooltipTrigger,
-  } from "@/components/ui/tooltip"
-  import { TooltipProvider } from "@/components/ui/tooltip"; 
-  import {
-    File,
+} from "@/components/ui/tooltip";
+import { TooltipProvider } from "@/components/ui/tooltip"; 
+import {
     Home,
-    LineChart,
-    ListFilter,
-    MoreHorizontal,
-    Package,
     Package2,
-    PanelLeft,
-    PlusCircle,
-    Search,
     Settings,
-    ShoppingCart,
-    Users2,
-  } from "lucide-react"
-  import Link from "next/link"
+} from "lucide-react";
+import Link from "next/link";
 import AxiosInstance from '@/lib/AxiosInstance';
-function page() {
-  const [listing, setListing] = useState([]);
 
-  const fetchData = useCallback(async () => {
-    try {
-      const response = await AxiosInstance.get("listing/get_all_listings");
-      setListing(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
+function Page() {
+    const [listing, setListing] = useState([]);
 
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+    const fetchData = useCallback(async () => {
+        try {
+            const response = await AxiosInstance.get("listing/get_all_listings");
+            setListing(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
     return (
         <div>
-         <TooltipProvider>
-      <aside className="fixed inset-y-0 left-0 z-10 flex-col hidden border-r w-14 bg-background md:flex">
-        <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-          <Link
-            href="#"
-            className="flex items-center justify-center gap-2 text-lg font-semibold rounded-full group h-9 w-9 shrink-0 bg-primary text-primary-foreground md:h-8 md:w-8 md:text-base"
-          >
-            <Package2 className="w-4 h-4 transition-all group-hover:scale-110" />
-            <span className="sr-only">Acme Inc</span>
-          </Link>
-          
-          {/* Tooltip examples */}
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex items-center justify-center transition-colors rounded-lg h-9 w-9 text-muted-foreground hover:text-foreground md:h-8 md:w-8"
-              >
-                <Home className="w-5 h-5" />
-                <span className="sr-only">Dashboard</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Dashboard</TooltipContent>
-          </Tooltip>
+            <TooltipProvider>
+                <aside className="fixed inset-y-0 left-0 z-10 flex-col hidden border-r w-14 bg-background md:flex">
+                    <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+                        <Link
+                            href="#"
+                            className="flex items-center justify-center gap-2 text-lg font-semibold rounded-full group h-9 w-9 shrink-0 bg-primary text-primary-foreground md:h-8 md:w-8 md:text-base"
+                        >
+                            <Package2 className="w-4 h-4 transition-all group-hover:scale-110" />
+                            <span className="sr-only">Acme Inc</span>
+                        </Link>
+                        
+                        {/* Tooltip examples */}
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="#"
+                                    className="flex items-center justify-center transition-colors rounded-lg h-9 w-9 text-muted-foreground hover:text-foreground md:h-8 md:w-8"
+                                >
+                                    <Home className="w-5 h-5" />
+                                    <span className="sr-only">Dashboard</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Dashboard</TooltipContent>
+                        </Tooltip>
 
-          {/* Repeat for other Tooltips... */}
+                        {/* Repeat for other Tooltips... */}
 
-        </nav>
-        <nav className="flex flex-col items-center gap-4 px-2 mt-auto sm:py-5">
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Link
-                href="#"
-                className="flex items-center justify-center transition-colors rounded-lg h-9 w-9 text-muted-foreground hover:text-foreground md:h-8 md:w-8"
-              >
-                <Settings className="w-5 h-5" />
-                <span className="sr-only">Settings</span>
-              </Link>
-            </TooltipTrigger>
-            <TooltipContent side="right">Settings</TooltipContent>
-          </Tooltip>
-        </nav>
-      </aside>
-    </TooltipProvider>
-           {/* <div className="z-20 drawer drawer-end">
-  <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content">
-    
-    <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary">Open drawer</label>
-  </div>
-  <div className="drawer-side">
-    <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
-    <ul className="min-h-full p-4 menu bg-base-200 text-base-content w-80">
-     
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-    </ul>
-  </div>
-</div> */}
-<div className='grid w-[92vw] h-full justify-center overflow-y-auto md:ml-24'>
-{listing.map((elem,index)=>(<div key={index} className="mx-4 my-4 shadow-xl card bg-base-100 md:w-[85vw] md: h-[40vh]">
-  <div className=" card-body">
-    <h2 className="text-3xl card-title ">{elem?.topic}</h2>
-    <p className='text-xl '>{elem.description}</p>
-    <p className='text-lg '> domain:{elem.domain}</p>
-    <div className="justify-end card-actions">
-      <button className="btn btn-primary">{`Pay now ₹${elem?.sessionPrice}`}</button>
-    </div>
-  </div>
-</div>))}
-
-</div>
+                    </nav>
+                    <nav className="flex flex-col items-center gap-4 px-2 mt-auto sm:py-5">
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <Link
+                                    href="#"
+                                    className="flex items-center justify-center transition-colors rounded-lg h-9 w-9 text-muted-foreground hover:text-foreground md:h-8 md:w-8"
+                                >
+                                    <Settings className="w-5 h-5" />
+                                    <span className="sr-only">Settings</span>
+                                </Link>
+                            </TooltipTrigger>
+                            <TooltipContent side="right">Settings</TooltipContent>
+                        </Tooltip>
+                    </nav>
+                </aside>
+            </TooltipProvider>
+            <div className='grid w-[92vw] h-full justify-center overflow-y-auto md:ml-24'>
+                {listing.map((elem, index) => (
+                    <div key={index} className="mx-4 my-4 shadow-xl card bg-base-100 md:w-[85vw] md:h-[40vh]">
+                        <div className="card-body">
+                            <h2 className="text-3xl card-title">{elem?.topic}</h2>
+                            <p className='text-xl'>{elem.description}</p>
+                            <p className='text-lg'>domain: {elem.domain}</p>
+                            <p className='text-lg'>mentor name: {elem.mentorId.username}</p>
+                            <p className='text-lg'>mentor email: {elem.mentorId.email}</p>
+                            <div className="justify-end card-actions">
+                                <button className="btn btn-primary">{`Pay now ₹${elem?.sessionPrice}`}</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
 
-export default page
+export default Page;
--- frontend/src/app/mentor/dashboard/page.jsx
+++ frontend/src/app/mentor/dashboard/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image"
 import Link from "next/link"
 import {
@@ -76,11 +77,33 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 import { TooltipProvider } from "@/components/ui/tooltip"; 
+import { useState,useEffect } from "react"
+import AxiosInstance from "@/lib/AxiosInstance"
+import { useSelector,useDispatch } from "react-redux"
+import { loadUser } from "@/features/todo/Slice";
 // import EventDialog from "@/app/component/Session"
 export const description =
   "An orders dashboard with a sidebar navigation. The sidebar has icon navigation. The content area has a breadcrumb and search in the header. The main area has a list of recent orders with a filter and export button. The main area also has a detailed view of a single order with order details, shipping information, billing information, customer information, and payment information."
 
-export default function Dashboard() {
+export default function Page() {
+  const [listing,setListing]=useState([]);
+  const dispatch= useDispatch()
+useEffect(() => {
+    const unsubscribe = dispatch(loadUser());
+    return () => unsubscribe(); 
+  }, [dispatch]);
+  const userId= useSelector(state=>state.auth.currentuser?.uid);
+  const fetchData= async ()=>{
+    try {
+       const response= await AxiosInstance.get(`/listing/get_listings_by_mentor/${userId}`);
+       setListing(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(()=>{
+fetchData()
+  },[fetchData])
   return (
     <div className="flex flex-col w-full min-h-screen bg-muted/40">
        <TooltipProvider>
@@ -351,166 +374,27 @@ export default function Dashboard() {
                         </TableRow>
                       </TableHeader>
                       <TableBody>
-                        <TableRow className="bg-accent">
+                        {listing.map((elem,index)=>(<TableRow key={index} className="bg-accent">
                           <TableCell>
-                            <div className="font-medium">Liam Johnson</div>
+                            <div className="font-medium">{elem?.topic}</div>
                             <div className="hidden text-sm text-muted-foreground md:inline">
                               liam@example.com
                             </div>
                           </TableCell>
                           <TableCell className="hidden sm:table-cell">
-                            Sale
+                            {elem.domain}
                           </TableCell>
                           <TableCell className="hidden sm:table-cell">
                             <Badge className="text-xs" variant="secondary">
-                              Fulfilled
+                              {elem?.sessionStatus}
                             </Badge>
                           </TableCell>
                           <TableCell className="hidden md:table-cell">
-                            2023-06-23
+                            {elem.start}
                           </TableCell>
-                          <TableCell className="text-right">$250.00</TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell>
-                            <div className="font-medium">Olivia Smith</div>
-                            <div className="hidden text-sm text-muted-foreground md:inline">
-                              olivia@example.com
-                            </div>
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            Refund
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            <Badge className="text-xs" variant="outline">
-                              Declined
-                            </Badge>
-                          </TableCell>
-                          <TableCell className="hidden md:table-cell">
-                            2023-06-24
-                          </TableCell>
-                          <TableCell className="text-right">$150.00</TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell>
-                            <div className="font-medium">Noah Williams</div>
-                            <div className="hidden text-sm text-muted-foreground md:inline">
-                              noah@example.com
-                            </div>
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            Subscription
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            <Badge className="text-xs" variant="secondary">
-                              Fulfilled
-                            </Badge>
-                          </TableCell>
-                          <TableCell className="hidden md:table-cell">
-                            2023-06-25
-                          </TableCell>
-                          <TableCell className="text-right">$350.00</TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell>
-                            <div className="font-medium">Emma Brown</div>
-                            <div className="hidden text-sm text-muted-foreground md:inline">
-                              emma@example.com
-                            </div>
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            Sale
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            <Badge className="text-xs" variant="secondary">
-                              Fulfilled
-                            </Badge>
-                          </TableCell>
-                          <TableCell className="hidden md:table-cell">
-                            2023-06-26
-                          </TableCell>
-                          <TableCell className="text-right">$450.00</TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell>
-                            <div className="font-medium">Liam Johnson</div>
-                            <div className="hidden text-sm text-muted-foreground md:inline">
-                              liam@example.com
-                            </div>
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            Sale
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            <Badge className="text-xs" variant="secondary">
-                              Fulfilled
-                            </Badge>
-                          </TableCell>
-                          <TableCell className="hidden md:table-cell">
-                            2023-06-23
-                          </TableCell>
-                          <TableCell className="text-right">$250.00</TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell>
-                            <div className="font-medium">Liam Johnson</div>
-                            <div className="hidden text-sm text-muted-foreground md:inline">
-                              liam@example.com
-                            </div>
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            Sale
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            <Badge className="text-xs" variant="secondary">
-                              Fulfilled
-                            </Badge>
-                          </TableCell>
-                          <TableCell className="hidden md:table-cell">
-                            2023-06-23
-                          </TableCell>
-                          <TableCell className="text-right">$250.00</TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell>
-                            <div className="font-medium">Olivia Smith</div>
-                            <div className="hidden text-sm text-muted-foreground md:inline">
-                              olivia@example.com
-                            </div>
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            Refund
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            <Badge className="text-xs" variant="outline">
-                              Declined
-                            </Badge>
-                          </TableCell>
-                          <TableCell className="hidden md:table-cell">
-                            2023-06-24
-                          </TableCell>
-                          <TableCell className="text-right">$150.00</TableCell>
-                        </TableRow>
-                        <TableRow>
-                          <TableCell>
-                            <div className="font-medium">Emma Brown</div>
-                            <div className="hidden text-sm text-muted-foreground md:inline">
-                              emma@example.com
-                            </div>
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            Sale
-                          </TableCell>
-                          <TableCell className="hidden sm:table-cell">
-                            <Badge className="text-xs" variant="secondary">
-                              Fulfilled
-                            </Badge>
-                          </TableCell>
-                          <TableCell className="hidden md:table-cell">
-                            2023-06-26
-                          </TableCell>
-                          <TableCell className="text-right">$450.00</TableCell>
-                        </TableRow>
+                          <TableCell className="text-right">{` ₹ ${elem?.sessionPrice}`}</TableCell>
+                        </TableRow>))}
+                        
                       </TableBody>
                     </Table>
                   </CardContent>
