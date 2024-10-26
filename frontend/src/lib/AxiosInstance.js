a433a71e2bf699faa2336cee0bb9c0ff8d588019
Aayushchauhan05 <140552699+Aayushchauhan05@users.noreply.github.com>
2024-10-26 20:27:16 +0530

    change in env

--- frontend/src/lib/AxiosInstance.js
+++ frontend/src/lib/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"; 
 
 const AxiosInstance = axios.create({
-  baseURL : "http://127.0.0.1:8080/" ,
+  baseURL : process.env.NEXT_BASE_URL ,
   headers: {
 //  Authorization: `<Your Auth Token>`,
    " Content-Type": "application/json",
